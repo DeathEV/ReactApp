f5cb18551ee02338ecdc9e5eb15dc220ada2b8f3
phamq <phamquocthang2001@gmail.com>
2022-04-07 11:24:47 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-import {useNavigate} from "react-router-dom";
-import background from "./images/web-image-background.jpg"
 import RouterBase from "./routers/routerBase";
 import Home from "./view/Home";
 
 export default function App() {
     const token = localStorage.getItem('access_token');
-    const routerPush = useNavigate();
-    const logout = function (){
-      localStorage.removeItem('access_token') ;
-      routerPush("/login");
-    };
     if(token){
         return(
             <Home />
--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
+import {I18nextProvider} from "react-i18next";
+import i18next from "i18next";
+
+i18next.init({
+    interpolation: { escapeValue: false },
+});
 
 ReactDOM.render(
   <React.StrictMode>
       <BrowserRouter>
-          <App />
+          <I18nextProvider i18n={i18next}>
+              <App />
+          </I18nextProvider>
       </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
